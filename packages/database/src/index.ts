4ff3e5945ffd95828cbcbc7b4ade37f810f67ad1
Subash Dhungel <subash@kali.subash>
2026-04-02 04:37:48 -0400

    feat(database): setup core database module and service

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,5 @@
 // packages/database/src/index.ts
 export { PrismaService } from './prisma.service.js'
 export { PrismaModule } from './prisma.module.js'
+export { DatabaseModule } from './database.module.js'
 export * from '../generated/prisma/index.js'
