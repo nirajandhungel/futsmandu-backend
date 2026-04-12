faf1edc09bf6521ef93f89376f06dc36fbf4a19f
Nirajan Dhungel <subashdhungel555@gmail.com>
2026-04-12 20:44:53 +0545

    refactor(player-api): complete removal of legacy player api

--- apps/player-api/src/common/decorators/index.ts
+++
@@ -1,2 +0,0 @@
-// apps/player-api/src/common/decorators/index.ts
-export { CurrentUser, Public } from '@futsmandu/auth'
--- apps/player-api/src/modules/discovery/discovery.module.ts
+++
@@ -1,6 +0,0 @@
-// apps/player-api/src/modules/discovery/discovery.module.ts
-import { Module } from '@nestjs/common'
-import { DiscoveryService } from './discovery.service.js'
-import { DiscoveryController } from './discovery.controller.js'
-@Module({ providers: [DiscoveryService], controllers: [DiscoveryController] })
-export class DiscoveryModule {}
