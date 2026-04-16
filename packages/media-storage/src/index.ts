e3bf044bff33343df0de33146c3a3c2f22ac2347
Nirajan Dhungel <subashdhungel555@gmail.com>
2026-04-16 13:41:22 +0545

    refactor(media-storage): reorganize exports and storage module configuration

--- packages/media-storage/src/index.ts
+++ packages/media-storage/src/index.ts
@@ -1,1 +1,8 @@
-export { StorageService, type StorageConfig, type SignedUploadOptions, type StorageObjectMetadata } from './storage.service.js'
+export { StorageModule } from './storage.module.js';
+
+export {
+  StorageService,
+  type StorageConfig,
+  type SignedUploadOptions,
+  type StorageObjectMetadata,
+} from './storage.service.js';
--- packages/media-storage/src/storage.module.ts
+++ packages/media-storage/src/storage.module.ts
@@ -1,22 +1,22 @@
 // packages/media-storage/src/storage.module.ts
 
-import { Global, Module, DynamicModule } from '@nestjs/common'
-import { StorageService, type StorageConfig } from './storage.service.js'
-import { ENV } from '@futsmandu/utils'
+import { Global, Module, DynamicModule } from '@nestjs/common';
+import { StorageService, type StorageConfig } from './storage.service.js';
+import { ENV } from '@futsmandu/utils';
 
-export const STORAGE_CONFIG_TOKEN = 'STORAGE_CONFIG'
+export const STORAGE_CONFIG_TOKEN = 'STORAGE_CONFIG';
 
 function defaultConfig(): StorageConfig {
   return {
-    endpoint:               ENV['S3_ENDPOINT']    || '',
-    region:                 ENV['S3_REGION']      || 'auto',
-    accessKeyId:            ENV['S3_ACCESS_KEY']  || '',
-    secretAccessKey:        ENV['S3_SECRET_KEY']  || '',
-    bucket:                 ENV['S3_BUCKET']      || '',
-    enableCache:            true,
-    defaultGetExpirySeconds: 3_600,
-    forcePathStyle:         ENV['S3_FORCE_PATH_STYLE'] === 'true',
-  }
+    endpoint: ENV['S3_ENDPOINT'] || '',
+    region: ENV['S3_REGION'] || 'auto',
+    accessKeyId: ENV['S3_ACCESS_KEY'] || '',
+    secretAccessKey: ENV['S3_SECRET_KEY'] || '',
+    bucket: ENV['S3_BUCKET'] || '',
+    enableCache: true,
+    defaultGetExpirySeconds: 3600,
+    forcePathStyle: ENV['S3_FORCE_PATH_STYLE'] === 'true',
+  };
 }
 
 @Global()
@@ -27,16 +27,16 @@ export class StorageModule {
       module: StorageModule,
       providers: [
         {
-          provide:    STORAGE_CONFIG_TOKEN,
+          provide: STORAGE_CONFIG_TOKEN,
           useFactory: (): StorageConfig => ({ ...defaultConfig(), ...overrides }),
         },
         {
-          provide:    StorageService,
+          provide: StorageService,
           useFactory: (config: StorageConfig) => new StorageService(config),
-          inject:     [STORAGE_CONFIG_TOKEN],
+          inject: [STORAGE_CONFIG_TOKEN],
         },
       ],
       exports: [StorageService],
-    }
+    };
   }
 }
